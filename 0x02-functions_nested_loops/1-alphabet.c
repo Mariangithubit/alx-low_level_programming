931f1198c63e6d3dbc401cd21168c48c94828002
Mariangithubit <metsyb1@hotmail.com>
2023-03-21 15:16:37 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+_putchar('\n');
+}
