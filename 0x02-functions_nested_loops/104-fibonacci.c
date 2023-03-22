8b5c21803fd0f7537bae514765a010908c9541da
Mariangithubit <metsyb1@hotmail.com>
2023-03-22 15:53:33 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * numLength - returns the length of string
+ * @num: opened number
+ * Return: 0
+ */
+int numLength(int num)
+{
+	int length = 0;
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+/**
  * main - prints the first 98 Fibonacci numbers
  * Description: prints the first 98 Fibonacci numbers
  * Return: 0
@@ -15,7 +32,7 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		initial0s = mx - 1 - f1;
+		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%d", 0);
