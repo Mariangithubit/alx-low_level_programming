9426998df5eb4a56cb70c8a2544c1528655173c5
Mariangithubit <metsyb1@hotmail.com>
2023-03-23 19:14:13 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_square -\ prints a square
+ * @size: the size of the square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int r, c;
+
+	for (r = 1; r <= size; r++)
+	{
+		for (c = 1; c <= size; c++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
