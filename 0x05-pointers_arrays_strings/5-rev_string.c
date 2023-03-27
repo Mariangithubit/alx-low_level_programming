08565b59c96135611409e5022f53ae9471a8f06c
Mariangithubit <metsyb1@hotmail.com>
2023-03-27 16:41:01 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: length of string
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	int i = 0;
+	char temp;
+
+	while (s[i++])
+		l++;
+	for (i = l - 1; i >= l / 2; i--)
+		{
+			temp = s[i];
+			s[i] = s[l - 1 - i];
+			s[l - 1 - i] = temp;
+		}
+}
