6192e6e6b35b0565f9fc6ab383ddb2463660c3c0
Mariangithubit <metsyb1@hotmail.com>
2023-03-27 16:43:56 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,9 +13,9 @@ void rev_string(char *s)
 	while (s[i++])
 		l++;
 	for (i = l - 1; i >= l / 2; i--)
-		{
-			temp = s[i];
-			s[i] = s[l - 1 - i];
-			s[l - 1 - i] = temp;
-		}
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
 }
