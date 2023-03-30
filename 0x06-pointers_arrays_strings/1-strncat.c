6e7c295580caa277e6de2c1acaf6f8e6b928d8a5
Mariangithubit <metsyb1@hotmail.com>
2023-03-30 04:30:14 -0700

    1-strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[destlen + i] = src[i];
 	return (dest);
 }
