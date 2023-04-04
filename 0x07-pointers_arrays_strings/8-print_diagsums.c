cbc731aa32da8ab3f4343f57c9da5a5358987a7f
Mariangithubit <metsyb1@hotmail.com>
2023-04-03 18:38:18 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-        int sum1 = 0;
-        int sum2 = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += i;
+		sum1 += a[i];
 		sum2 += a[size - i - 1];
 		a += size;
 	}
