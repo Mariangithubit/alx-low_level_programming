7d0dcd32edb0ced6a19080ed13beb161055c7ce8
Mariangithubit <metsyb1@hotmail.com>
2023-04-09 12:11:01 -0700

    the arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main -  prints its name
+ * @argc: the count of argument
+ * @argv: the value of argument
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
