47876859c3cf0d8d14d53d7d371c22d7b2d84b5f
Mariangithubit <metsyb1@hotmail.com>
2023-04-09 18:14:50 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,11 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, k = 0, m = atoi(argv[1]);
-	int c[] = {25, 10, 5, 2, 1};
-
 	if (argc == 2)
 	{
+		int i, k = 0, m = atoi(argv[1]);
+		int c[] = {25, 10, 5, 2, 1};
+		
 		for (i = 0; i < 5; i++)
 		{
 			if (m >= c[i])
