1ab66ca1e8f5cf8d6a6a5e488a0e081181966d1c
Mariangithubit <metsyb1@hotmail.com>
2023-04-11 12:57:15 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,7 +4,7 @@
  * main - make change for an amount of money.
  * @argc: the counter
  * @argv: the value
- * return: 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	{
 		int i, k = 0, m = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
-		
+
 		for (i = 0; i < 5; i++)
 		{
 			if (m >= cents[i])
