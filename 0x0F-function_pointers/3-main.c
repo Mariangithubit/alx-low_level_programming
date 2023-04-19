83c5b234b01be0b7c743d21e3964090c982e96b2
Mariangithubit <metsyb1@hotmail.com>
2023-04-19 15:19:21 -0700

    3-calc.h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+/**
+ * main - the main function
+ * @argc: the osurce of argument
+ * @argv: the vector of argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int (*op_fun)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n");
+	exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_fun = get_op_func(argv[2]);
+	if (op_fun == NULL)
+		printf("Error\n");
+	exit(99);
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n");
+	exit(100);
+	printf("%d\n", op_fun(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - sum two nums
+ * @a: num 1
+ * @b: num 2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract two nums
+ * @a: num 1
+ * @b: num 2
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mutipule two nums
+ * @a: num 1
+ * @b: num 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two nums
+ * @a: num 1
+ * @b: num 2
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *  op_mod - module two nums
+ * @a: num 1
+ * @b: num 2
+ * Return: the remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
