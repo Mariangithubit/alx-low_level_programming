242adbe9a00a872a017c65ec00fa28a521962a04
Mariangithubit <metsyb1@hotmail.com>
2023-04-28 10:26:59 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - free a list
+ * @head: node
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while ((node = head) != NULL)
+	{
+		head = head->next;
+		free(node->str);
+		free(node);
+	}
+}
