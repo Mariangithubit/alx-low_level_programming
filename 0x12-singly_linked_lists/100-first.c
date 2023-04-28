a6808010d3614647c693e06d65f0eea5caf1be59
Mariangithubit <metsyb1@hotmail.com>
2023-04-28 14:07:01 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void _first(void) __attribute__ ((constructor));
+/**
+ * _first - printed function
+ * Return: void
+ */
+void _first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
