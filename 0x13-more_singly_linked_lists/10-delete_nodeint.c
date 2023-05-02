21933bfe165b0488f41939684dd412fad4050bf0
Mariangithubit <metsyb1@hotmail.com>
2023-05-02 15:56:33 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if(i == index)
+		if (i == index)
 		{
 			curr_node->next = node->next;
 			free(node);
