fb3e794bf289f3b9d1c4cf21732f5a2226b27eb0
Mariangithubit <metsyb1@hotmail.com>
2023-05-08 15:19:26 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: the name of teh file
+ * @letters: the number of letters should be read
+ * Return:  the actual number of letters or 0 if  filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t frd, fwr;
+	char *buf;
+	int fb;
+
+	if (filename == NULL)
+		return (0);
+	fb = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	frd = read(fb, buf, letters);
+	fwr = write(STDOUT_FILENO, buf, frd);
+	close(fb);
+	free(buf);
+	return (fwr);
+}
