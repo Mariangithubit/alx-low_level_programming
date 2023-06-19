445fc491fcf81544b180e33c2f6919dbe5987b98
Mariangithubit <metsyb1@hotmail.com>
2023-06-19 14:23:37 -0700

    100-operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "operation.h"
+
+/**
+ * add - add calc
+ * @a: first integer
+ * @b: second integer
+ * Return: claculation of 2 numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction calc
+ * @a: first integer
+ * @b: second integer
+ * Return: claculation of 2 numbers
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiply calc
+ * @a: first integer
+ * @b: second integer
+ * Return: claculation of 2 numbers
+ */
+int mul(int a, int b)
+{       
+        return (a * b);
+}
+
+/**
+ * div - division calc
+ * @a: first integer
+ * @b: second integer
+ * Return: claculation of 2 numbers
+ */
+int div(int a, int b)
+{       
+        return (a / b);
+}
+
+/**
+ * mod - module calc
+ * @a: first integer
+ * @b: second integer
+ * Return: claculation of 2 numbers
+ */
+int mod(int a, int b)
+{       
+        return (a % b);
+}
