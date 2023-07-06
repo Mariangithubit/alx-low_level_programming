23d09ccecee0a93fc7c8b55331f701b5bc57d606
Mariangithubit <metsyb1@hotmail.com>
2023-07-06 15:45:02 -0700

    index of a key.

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - dds an element to the hash table.
+ * @ht: the hash tableto be add
+ * @key: the new key.
+ * @value: the value of the key.
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_hash_node = NULL;
+	hash_node_t *temp = NULL;
+	unsigned long int index = 0;
+
+	if (!h || !key || !(*key) || !value)
+		return (0);
+
+	while (temp && strcmp(temp->key, key) != 0)
+		temp = temp->next;
+
+	if (temp)
+	{
+		free(temp->value);
+		temp->value = strdup(value);
+		return (1);
+	}
+
+	new_hash_node = malloc(sizeof(*new_hash_node));
+	if (!new_hash_node)
+		return (0);
+
+	new_hash_node->key = strdup(key);
+	new_hash_node->value = strdup(value);
+
+	new_hash_node->next = ht->array[index];
+	ht->array[index] = new_hash_node;
+
+	return (1);
+}
