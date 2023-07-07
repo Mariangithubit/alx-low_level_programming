38e6afb51ac81be4564202945305b286d773450f
Mariangithubit <metsyb1@hotmail.com>
2023-07-07 01:43:48 -0700

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !(*key) || !value)
 		return (0);
 
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
 	while (temp && strcmp(temp->key, key) != 0)
 		temp = temp->next;
 
