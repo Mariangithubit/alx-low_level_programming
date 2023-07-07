8e92a97d67a0f0b3d167b4bb69fd66b2dc2e531a
Mariangithubit <metsyb1@hotmail.com>
2023-07-07 02:51:24 -0700

    del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,7 +17,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		k = ht->array[i];
-		while((temp = k) != NULL)
+		while ((temp = k) != NULL)
 		{
 			k = k->next;
 			free(temp->key);
