b7245c0affd0973944fd3b29b2d684b0fc7ca7ac
Mariangithubit <metsyb1@hotmail.com>
2023-10-09 11:59:44 -0700

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALOGS_H_
+#define SEARCH_ALOGS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
